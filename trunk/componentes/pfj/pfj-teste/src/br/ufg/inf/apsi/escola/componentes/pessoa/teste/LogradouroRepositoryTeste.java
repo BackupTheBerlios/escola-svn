fcc88872e7b4227cbcab8c87a1ebadec919bd1d5
gilmararantes <gilmararantes@7111949f-1a0a-0410-bd7a-b606ef22a16a>
2007-09-04 02:33:49 +0000

    atualizacao de classes de teste

---
+++ trunk/componentes/pfj/pfj-teste/src/br/ufg/inf/apsi/escola/componentes/pessoa/teste/LogradouroRepositoryTeste.java
@@ -0,0 +1,153 @@
+package br.ufg.inf.apsi.escola.componentes.pessoa.teste;
+
+import org.jmock.Expectations;
+import org.jmock.Mockery;
+import org.jmock.integration.junit4.JMock;
+import org.jmock.integration.junit4.JUnit4Mockery;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import br.ufg.inf.apsi.escola.componentes.pessoa.modelo.Logradouro;
+import br.ufg.inf.apsi.escola.componentes.pessoa.repositorio.LogradouroRepository;
+import br.ufg.inf.apsi.escola.componentes.pessoa.util.EscolaException;
+import br.ufg.inf.apsi.escola.componentes.pessoa.util.LogradouroCadastradoException;
+import br.ufg.inf.apsi.escola.componentes.pessoa.util.LogradouroNaoEncontradoException;
+import br.ufg.inf.apsi.escola.componentes.pessoa.util.NenhumLogradouroEncontradoException;
+
+@RunWith(JMock.class)
+public class LogradouroRepositoryTeste {
+	private Logradouro log = new Logradouro();
+	private Mockery context = new JUnit4Mockery();
+	private LogradouroRepository lr = context.mock(LogradouroRepository.class);
+	/**
+	 * 
+	 */
+	@Test
+	public void testaIncluir(){
+		try {
+			context.checking(new Expectations(){{
+				one (lr).incluir(log);
+			}});
+		} catch (LogradouroCadastradoException lce) {
+			System.out.println(lce.getMessage());
+		}
+		
+		try {
+			lr.incluir(log);
+		} catch (LogradouroCadastradoException lce) {
+			System.out.println(lce.getMessage());
+		}
+		
+		context.assertIsSatisfied();
+				
+	}
+	/**
+	 * 
+	 */
+	@Test
+	public void testaRemover(){
+		try {
+			context.checking(new Expectations(){{
+				one (lr).remover(log.getId());
+			}});
+		} catch (EscolaException e) {
+			System.out.println(e.getMessage());
+		}
+		
+		try {
+			lr.remover(log.getId());
+		} catch (EscolaException e) {
+			System.out.println(e.getMessage());
+		}
+		
+		context.assertIsSatisfied();		
+				
+	}
+	/**
+	 * 
+	 */
+	@Test
+	public void testaSalvar(){
+		
+		try {
+			context.checking(new Expectations(){{
+				one (lr).salvar(log);
+			}});
+		} catch (EscolaException e) {
+			System.out.println(e.getMessage());
+		}
+		
+		try {
+			lr.salvar(log);
+		} catch (EscolaException e) {
+			System.out.println(e.getMessage());
+		}
+		
+		context.assertIsSatisfied();				
+	}
+	/**
+	 * 
+	 */
+	@Test
+	public void testaConsultar(){
+		
+		try {
+			context.checking(new Expectations(){{
+				one (lr).consultar(log.getNome());
+			}});
+		} catch (LogradouroNaoEncontradoException lnee) {
+			System.out.println(lnee.getMessage());
+		}
+		
+		try {
+			lr.consultar(log.getNome());
+		} catch (LogradouroNaoEncontradoException lnee) {
+			System.out.println(lnee.getMessage());
+		}
+		
+		context.assertIsSatisfied();		
+	}
+	/**
+	 * 
+	 */
+	@Test
+	public void testaCarregar(){
+		
+		try {
+			context.checking(new Expectations(){{
+				one (lr).carregar(log.getId());
+			}});
+		} catch (LogradouroNaoEncontradoException lnee) {
+			System.out.println(lnee.getMessage());
+		}
+		
+		try {
+			lr.carregar(log.getId());
+		} catch (LogradouroNaoEncontradoException lnee) {
+			System.out.println(lnee.getMessage());
+		}
+		
+		context.assertIsSatisfied();
+	}
+	/**
+	 * 
+	 */
+	@Test
+	public void testaListaTodos(){
+		try {
+			context.checking(new Expectations(){{
+				one (lr).listaTodos();
+			}});
+		} catch (NenhumLogradouroEncontradoException nlee) {
+			System.out.println(nlee.getMessage());
+		}
+		
+		try {
+			lr.listaTodos();
+		} catch (NenhumLogradouroEncontradoException nlee) {
+			System.out.println(nlee.getMessage());
+		}
+		
+		context.assertIsSatisfied();
+	}
+}
