857679ae3d5e017a4aaed25afc829d9034608aff
gilmararantes <gilmararantes@7111949f-1a0a-0410-bd7a-b606ef22a16a>
2007-10-28 12:28:20 +0000

    adição do teste da implementação do método toString() na interface PessoaService

---
+++ trunk/componentes/pfj/pfj-data/src/br/ufg/inf/apsi/escola/componentes/pessoa/repositorio/teste/ToString.java
@@ -0,0 +1,51 @@
+package br.ufg.inf.apsi.escola.componentes.pessoa.repositorio.teste;
+
+import br.ufg.inf.apsi.escola.componentes.pessoa.modelo.excecoes.PessoaNaoEncontradaException;
+import br.ufg.inf.apsi.escola.componentes.pessoa.negocio.PessoaService;
+import br.ufg.inf.apsi.escola.componentes.pessoa.negocio.PessoaServiceImpl;
+import br.ufg.inf.apsi.escola.componentes.pessoa.repositorio.jpa.hibernate.BairroRepositoryImpl;
+import br.ufg.inf.apsi.escola.componentes.pessoa.repositorio.jpa.hibernate.CidadeRepositoryImpl;
+import br.ufg.inf.apsi.escola.componentes.pessoa.repositorio.jpa.hibernate.CriaPersistenciaGeral;
+import br.ufg.inf.apsi.escola.componentes.pessoa.repositorio.jpa.hibernate.DocumentoRepositoryImpl;
+import br.ufg.inf.apsi.escola.componentes.pessoa.repositorio.jpa.hibernate.EmailRepositoryImpl;
+import br.ufg.inf.apsi.escola.componentes.pessoa.repositorio.jpa.hibernate.EnderecoRepositoryImpl;
+import br.ufg.inf.apsi.escola.componentes.pessoa.repositorio.jpa.hibernate.EstadoRepositoryImpl;
+import br.ufg.inf.apsi.escola.componentes.pessoa.repositorio.jpa.hibernate.LogradouroRepositoryImpl;
+import br.ufg.inf.apsi.escola.componentes.pessoa.repositorio.jpa.hibernate.PaisRepositoryImpl;
+import br.ufg.inf.apsi.escola.componentes.pessoa.repositorio.jpa.hibernate.PessoaRepositoryImpl;
+import br.ufg.inf.apsi.escola.componentes.pessoa.repositorio.jpa.hibernate.TelefoneRepositoryImpl;
+
+/**
+ * classe definida para testar a implementação do método toString() da interface PessoaService
+ * @author gilmar
+ *
+ */
+public class ToString {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String args[]){
+		CriaPersistenciaGeral cpg = new CriaPersistenciaGeral();
+		PessoaService ps = new PessoaServiceImpl();
+		ps.setBairroRepository(new BairroRepositoryImpl());
+		ps.setCidadeRepository(new CidadeRepositoryImpl());
+		ps.setDocumentoRepository(new DocumentoRepositoryImpl());
+		ps.setEmailRepository(new EmailRepositoryImpl());
+		ps.setEnderecoRepository(new EnderecoRepositoryImpl());
+		ps.setEstadoRepository(new EstadoRepositoryImpl());
+		ps.setLogradouroRepository(new LogradouroRepositoryImpl());
+		ps.setPaisRepository(new PaisRepositoryImpl());
+		ps.setPessoaRepository(new PessoaRepositoryImpl());
+		ps.setTelefoneRepository(new TelefoneRepositoryImpl());
+
+		try {
+			System.out.println(ps.toString(1L));
+		} catch (PessoaNaoEncontradaException ee) {
+			ee.printStackTrace();
+			ee.getCause().getMessage();
+		}
+
+		cpg.finaliza();
+	}
+}
