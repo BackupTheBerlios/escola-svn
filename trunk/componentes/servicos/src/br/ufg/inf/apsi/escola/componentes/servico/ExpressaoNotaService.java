39d44133e53ddbbb691a45c9b05791b4f7fa8704
grpereira <grpereira@7111949f-1a0a-0410-bd7a-b606ef22a16a>
2007-11-02 18:23:02 +0000

    Atualizar nova forma de validar expressão de nota, usando JEL. Deixar comentado método de calcular notas, que deverá ficar em uso no momento que integrar os componentes.

--- trunk/componentes/servicos/src/br/ufg/inf/apsi/escola/componentes/servico/ExpressaoNotaService.java
+++ trunk/componentes/servicos/src/br/ufg/inf/apsi/escola/componentes/servico/ExpressaoNotaService.java
@@ -1,26 +1,27 @@
-package br.ufg.inf.apsi.escola.componentes.servico;
+package src.br.ufg.inf.apsi.escola.componentes.servico;
 
-/*
- * pacote para que seja possivel utilizar expressões em Java
- */
-import java.util.regex.*;
-
-/*
- * Aqui deverá ser importado o pacote que contém os serviços de notas de alunos. 
- */
+import gnu.jel.CompilationException;
+import gnu.jel.CompiledExpression;
+import gnu.jel.Evaluator;
+import gnu.jel.Library;
 
 public class ExpressaoNotaService {
 
-	/*
-	 * Método para validar a expressão, aqui existe um contrato onde as notas
-	 * devem ser o caracter N (maiusculo) necessariamente e só pode conter 
-	 * caracteres para cálculos matemáticos (p.ex. (), [], *+-/e os números)
-	 */
 	public boolean validar(String expressao) throws Exception {
 
-		/* esse laço verificará se a expressão têm caracteres válidos...
-		 * caso não tenha, uma exceção sera levantada.
-		 */ 
+		/*
+		 * Verifica se uma expressão foi passada como parâmetro.
+		 */
+		if (expressao.length() == 0) {
+			Exception e = new Exception("Uma Expressão deve ser informada!");
+			throw e;
+		}
+
+		/*
+		 * Este laço verificar se a expressão passada contém caracteres não 
+		 * válidos para a expressão. p.ex. uma letra A ou B etc., sendo que
+		 * a única letra permitida é a letra N(maiscula ou minuscula).  
+		 */
 		for (int i = 0; i < expressao.length(); i++) {
 			if (expressao.substring(i, i + 1).equals("(")
 					|| expressao.substring(i, i + 1).equals(")")
@@ -30,6 +31,8 @@ public class ExpressaoNotaService {
 					|| expressao.substring(i, i + 1).equals("+")
 					|| expressao.substring(i, i + 1).equals("-")
 					|| expressao.substring(i, i + 1).equals("/")
+					|| expressao.substring(i, i + 1).equals("^")
+					|| expressao.substring(i, i + 1).equals("%")
 					|| expressao.substring(i, i + 1).equals("0")
 					|| expressao.substring(i, i + 1).equals("1")
 					|| expressao.substring(i, i + 1).equals("2")
@@ -40,23 +43,54 @@ public class ExpressaoNotaService {
 					|| expressao.substring(i, i + 1).equals("7")
 					|| expressao.substring(i, i + 1).equals("8")
 					|| expressao.substring(i, i + 1).equals("9")
+					|| expressao.substring(i, i + 1).equals("n")
 					|| expressao.substring(i, i + 1).equals("N")) {
 			}
 			else {
-				Exception e = new Exception("ERRO - Expressão com caracteres não válidos");
+				Exception e = new Exception("ERRO - Expressão com caracteres inválidos");
 				throw e;
 			}
 		}
+	
 		/*
-		 * Aqui será validado se a expressão é válida
+		 * Este laço retirará a(s) letra(s) Nn da expressão
+		 * para submeter ao JEL apenas a expressao pura (limpa de letras)
 		 */
-		Pattern padrao = Pattern.compile("\\(|\\)|\\*|\\/|\\+|\\-|0-9|N");
-		Matcher pesquisa = padrao.matcher(expressao);
+		StringBuffer expressao_sb = new StringBuffer();
+		for (int i = 0; i < expressao.length(); i++) {
+			if (!expressao.substring(i, i + 1).equals("n") 
+					& !expressao.substring(i, i + 1).equals("N")) {
+				expressao_sb.append(expressao.substring(i, i + 1));
+			}
+		}
+		String expressao_limpa = expressao_sb.toString();
+		
+		/*
+		 * Verifica se a expressão é válida
+		 * Monta a biblioteca
+		 */
 
-		if (!pesquisa.find()) {
-			Exception e = new Exception("ERRO - Expressão inválida!");
+		Class[] staticLib = new Class[1];
+		try {
+			staticLib[0] = Class.forName("java.lang.Math");
+		} catch (ClassNotFoundException e) {
 			throw e;
-        }   
+		}
+		
+		Library lib = new Library(staticLib, null, null, null, null);
+		try {
+			lib.markStateDependent("random", null);
+		} catch (CompilationException e) {
+			throw e;
+		}
+
+		// Compila a expressao
+		CompiledExpression compilaExpressao = null;
+		try {
+			compilaExpressao = Evaluator.compile(expressao_limpa, lib);
+		} catch (CompilationException ce) {
+			throw ce;
+		}
 		return true;
 	}
 
@@ -64,22 +98,24 @@ public class ExpressaoNotaService {
 	 * Este método, retornará a nota do aluno na turma, se for possivel, ou seja,
 	 * se as notas estiverem todas lançadas.
 	 * obs: Este método deve ser sincronizado com os métodos corretos dos
-	 * outros pacotes de trabalho.
+	 * outros pacotes de trabalho no momento da integraçaõ dos componentes
+	 * por esse motivo ele esta comentado.
 	 */
-	public double calcularNotaAlunos(Object turma, Object aluno, String expressao) throws Exception {
+	
+	/*public double calcularNotaAlunos(int turma, int aluno) throws Exception {
 		Notas notas = new Notas(aluno, turma);
 		if (notas.getValores().length != turma.qtdeNotas()){
 			throw new Exception("Notas insuficientes para calcular média final!");
 		}		
 		int soma = 0;
 		for (int i = 0; i < turma.qtdeNotas(); i++) {
-			expressao = expressao.replaceAll("N"+(i+1), String.valueOf(notas.getValores()[i]));
+			turma.expressao = turma.expressao.replaceAll("N"+(i+1), String.valueOf(notas.getValores()[i]));
 		}
-		String[] expressaoAux = expressao.split("[\\/0-9]");
+		String[] expressaoAux = turma.expressao.split("[\\/0-9]");
 		for (int i = 0; i < expressaoAux.length; i++) {
 			String string = expressaoAux[i];
 		}
 		int media = soma/turma.qtdeNotas();
 		return media;
-	}
+	}*/
 }
